1a4a056d90fc55c3929643bbf0c7a4eaababbbc5
fidanmardanli <fidannm@code.edu.az>
2022-07-13 06:42:09 +0400

    Exercise about delegate

---
+++ Delegate Task/DelegateTask/DelegateTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DelegateTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Fidash", "Cavid", "Mahmud" };
+            CheckingWord(Checking, names);
+        }
+
+        public static bool Checking(string names)
+        {
+
+            return names != "Cavid";
+        }
+
+        public static void CheckingWord(Predicate<string> predicate, List<string> names)
+        {
+            foreach (string name in names)
+            {
+                if (predicate(name))
+                {
+                    Console.WriteLine(name);                }
+
+            }
+            
+        }
+    }
+}
